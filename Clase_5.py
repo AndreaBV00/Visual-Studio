22d39ff01434c98e67cb49a38e070ec3368f820f
AndreaBV00 <azulaitami@gmail.com>
2024-07-24 22:27:17 -0300

    chore: Add boolean variable to Clase_5.py

---
+++ Clase_5.py
@@ -0,0 +1,21 @@
+#valores lógicos son True y False (booleanos)
+variable = True
+print(variable)
+
+#True =1 y False = 0
+#Python considera que el 0 es False y cualquier otro número es True. 
+# signo de distinto !=
+# signo de igualdad ==
+
+#operaciones con booleanos
+
+resultado = 5 *  2 == 10 #
+print(resultado)    #True
+print(not resultado) #False
+
+
+print(False and True) #False
+print(True and True) #True, no se dan las dos condiciones, por eso es falsorint (False and False) #False
+print(False or True) #True
+print(False or False) #False
+print(True or True) #True

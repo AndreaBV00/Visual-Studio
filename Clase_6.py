68f08ad8b7b7bbc2d3e56fa966f590a23dcea98b
AndreaBV00 <azulaitami@gmail.com>
2024-07-29 21:25:53 -0300

    Refactor code to include conditional statements for user input validation and display in Clase_6.py

--- Clase_6.py
+++ Clase_6.py
@@ -5,5 +5,27 @@
 #4. Salida de datos
 #5. Fin
 
-name = 'Andrea'
-print('Hola', name)
+
+
+#Condicionales
+#Estructura de un condicional en Python
+# if condicion:
+#     bloque de código
+# else:
+#     bloque de código
+
+#Ejemplo de condicional
+#numero = 5
+#if numero > 0:
+#    print('El número es positivo')
+#else:  
+#    print('El número es negativo')
+
+name = input('Ingrese su nombre: ')
+if len(name) > 5:
+    print('Tu nombre tiene más de 5 caracteres')
+if name[0] == 'A':
+    print('Tu nombre empieza con la letra A')
+if name == name.capitalize():
+    print('Tu nombre está bien escrito')
+print('Finalizando la ejecución')
